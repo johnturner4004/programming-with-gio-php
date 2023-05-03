d62f246457c8ffae4c760068dc651d3a5aa086aa
John Turner <john@Johns-MacBook-Pro.local>
2023-05-03 06:14:12 -0500

    scalar types

--- index.php
+++ index.php
@@ -4,9 +4,28 @@
 
   // 4 Scalar Types
     // bool
+    $completed = true;
     // int
-    // float
+    $count = 75;
+    // float also double
+    $price = 1.75;
     // string
+    $greeting = 'Hello, John';
+
+    echo $completed . '<br />';
+    echo $count . '<br />';
+    echo $price . '<br />';
+    echo $greeting . '<br />';
+
+    echo gettype($completed) . '<br />';
+    echo gettype($count) . '<br />';
+    echo gettype($price) . '<br />';
+    echo gettype($greeting) . '<br />';
+
+    echo var_dump($completed) . '<br />';
+    echo var_dump($count) . '<br />';
+    echo var_dump($price) . '<br />';
+    echo var_dump($greeting) . '<br />';
 
   // 4 compound types
     // array
